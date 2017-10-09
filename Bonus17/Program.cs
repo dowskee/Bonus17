f00eda981ae7198e42061485a0904c728b31ff31
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-09 13:04:35 -0400

    Class and Main Edits

--- Bonus17/Program.cs
+++ Bonus17/Program.cs
@@ -10,6 +10,45 @@ namespace Bonus17
     {
         static void Main(string[] args)
         {
+            //create a class named CarApp that gets the user input, creates a car object, and displays the car information
+            Console.WriteLine("Welcome to the Grand Circus Motors admin console!");
+            int AmountofCars;
+            Console.WriteLine("How many cars are you entering:");
+            AmountofCars = int.Parse(Console.ReadLine());
+
+            //create an array to hold the objects 
+
+            Car[] CarApp = new Car[AmountofCars];
+
+            for (int i = 0; i < CarApp.Length; i++)
+            {
+                CarApp[i] = new Car(); //create an object 
+            }
+
+            //initialize
+
+            for (int i = 0; i < CarApp.Length; i++)
+            {
+                Console.WriteLine("Please enter the car's Make");
+                CarApp[i].Make = Console.ReadLine();
+
+                Console.WriteLine("Please enter the car's Model");
+                CarApp[i].Model = Console.ReadLine();
+
+                Console.WriteLine("Please enter the car's Year");
+                CarApp[i].Year = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the car's Price:");
+                CarApp[i].Price = double.Parse(Console.ReadLine()); //n2 for price writing
+            }
+            Console.WriteLine("Current Inventory:");
+            Console.WriteLine("Make \tModel \tYear \tPrice");
+
+            foreach (Car Element in CarApp)
+            {
+                Console.WriteLine($"{Element.Make} \t{Element.Model} \t{Element.Year} \t{Element.Price}");
+            }
+
         }
     }
 }
